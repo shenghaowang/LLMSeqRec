7fc8917277e627f967e39d1bdb3de732405e20ec
Shenghao Wang <wangshenghao1993@gmail.com>
2025-07-09 18:24:28 -0400

    Support items in both list and tensor

--- src/model/metrics.py
+++ src/model/metrics.py
@@ -1,14 +1,28 @@
+from typing import List
+
+import numpy as np
 import torch
 
 
-def compute_hit_rate(topk_items, true_item):
-    return int(true_item in topk_items)
-
-
-def compute_ndcg(topk_items, true_item):
-    if true_item in topk_items:
-        rank = (topk_items == true_item).nonzero(as_tuple=True)[0].item() + 1
-        return 1 / torch.log2(torch.tensor(rank + 1, dtype=torch.float))
+def compute_hit_rate(topk_items: torch.Tensor | List[int], true_item: int) -> int:
+    if isinstance(topk_items, torch.Tensor):
+        return int((topk_items == true_item).any())
 
     else:
-        return 0.0
+        return int(true_item in topk_items)
+
+
+def compute_ndcg(topk_items: torch.Tensor | List[int], true_item: int) -> float:
+    if isinstance(topk_items, torch.Tensor):
+        matches = (topk_items == true_item).nonzero(as_tuple=True)[0]
+        if len(matches) == 0:
+            return 0.0
+        rank = matches.item() + 1
+
+    else:
+        indices = np.where(np.array(topk_items) == true_item)[0]
+        if len(indices) == 0:
+            return 0.0
+        rank = indices[0] + 1
+
+    return float(1 / np.log2(rank + 1))
